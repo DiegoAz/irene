0aa888a108be053ec4866a082953d235fa1811b5
Diego <daalvarez@unbosque.edu.co>
2024-10-24 22:32:33 -0500

    Refactor navigation using the 11ty Navigation Plugin and global data

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,7 +1,9 @@
 const eleventyPostcss = require("@jgarber/eleventy-plugin-postcss");
-
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+const { data } = require("autoprefixer");
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPlugin(eleventyPostcss);
+    eleventyConfig.addPlugin(eleventyNavigationPlugin);
     eleventyConfig.addPassthroughCopy("src/js");
     eleventyConfig.setServerOptions({
         showAllHosts: true,
@@ -10,6 +12,7 @@ module.exports = function (eleventyConfig) {
         dir: {
             input: "src",
             output: "_site",
+            data: "_data",
         },
     };
 };
---
+++ src/_data/navigation.js
@@ -0,0 +1,64 @@
+module.exports = {
+  es: [
+    {
+      title: "Inicio",
+      url: "/",
+      key: "home"
+    },
+    {
+      title: "Sobre el proyecto",
+      url: "/acerca-de/",
+      key: "about"
+    },
+    {
+      title: "Currículo",
+      url: "/curriculum/",
+      key: "curriculum"
+    },
+    {
+      title: "Recursos didácticos",
+      url: "/recursos-didacticos/",
+      key: "resources"
+    },
+    {
+      title: "Guía",
+      url: "/guia/",
+      key: "guide"
+    },
+    {
+      title: "Testimonios",
+      key: "testimonials",
+      children: [
+        { title: "Fortaleza, 2024", url: "/testimonials/fortaleza-2024/" },
+        { title: "Tunja, 2023", url: "/testimonials/tunja-2023/" },
+        // ... más testimonios ...
+      ]
+    },
+    {
+      title: "Investigación",
+      url: "/investigacion/",
+      key: "research"
+    },
+    {
+      title: "Colaboradores",
+      url: "/colaboradores/",
+      key: "partners"
+    },
+    {
+      title: "Contacto",
+      url: "/contacto/",
+      key: "contact"
+    },
+    {
+      title: "Noticias",
+      url: "/noticias/",
+      key: "news"
+    },
+    {
+      title: "Apoya el proyecto",
+      url: "/apoya/",
+      key: "support"
+    }
+  ],
+  // Puedes agregar más idiomas aquí en el futuro
+};
