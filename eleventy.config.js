d4f38481965fe74a142b658af981410636359db3
Diego <daalvarez@unbosque.edu.co>
2024-11-02 09:08:30 -0500

    Add passthrough copy for videos

--- eleventy.config.js
+++ eleventy.config.js
@@ -7,6 +7,7 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPlugin(eleventyNavigationPlugin);
     eleventyConfig.addPassthroughCopy("src/js");
     eleventyConfig.addPassthroughCopy("src/img");
+    eleventyConfig.addPassthroughCopy("src/vid");
     eleventyConfig.setServerOptions({
         showAllHosts: true,
     });
