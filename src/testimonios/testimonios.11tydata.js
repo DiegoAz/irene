62a0b6b0f987497bd8d38ed3a1706df855e1393f
Diego <daalvarez@unbosque.edu.co>
2024-12-04 12:16:48 -0500

    Disable eleventyComputed: avoid the permalink that include city: city is for text in the page, it can contains accents

--- src/testimonios/testimonios.11tydata.js
+++ src/testimonios/testimonios.11tydata.js
@@ -1,18 +1,18 @@
 module.exports = {
   layout: "layouts/testimonial.liquid",
   tags: ["testimonio"],
-  eleventyComputed: {
-    permalink: data => `/testimonios/${data.city.toLowerCase()}-${data.year}/index.html`,
-    videos: data => {
-      if (!data.videos) return null;
-      return data.videos.map(video => {
-        if (typeof video === 'string') {
-          return {
-            url: video
-          };
-        }
-        return video;
-      });
-    }
-  }
+  // eleventyComputed: {
+  //   permalink: data => `/testimonios/${data.city.toLowerCase()}-${data.year}/index.html`,
+  //   videos: data => {
+  //     if (!data.videos) return null;
+  //     return data.videos.map(video => {
+  //       if (typeof video === 'string') {
+  //         return {
+  //           url: video
+  //         };
+  //       }
+  //       return video;
+  //     });
+  //   }
+  // }
 };
