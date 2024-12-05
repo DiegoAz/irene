b68cd07de5cd844e7600aac0ee38352e330fb1a9
Diego <daalvarez@unbosque.edu.co>
2024-12-04 20:56:12 -0500

    Edit images.three.src with the correct img

--- src/_data/guide.js
+++ src/_data/guide.js
@@ -13,56 +13,53 @@ module.exports = {
                 intro: "You can download the full guide and the guide for participants below",
                 all: {
                     text: "Complete guide",
-                    href: "#"
+                    href: "#",
                 },
                 participants: {
                     text: "Guide for the participants",
-                    href: "#"
-                }
-            }
+                    href: "#",
+                },
+            },
         ],
         es: [
             {
                 intro: "A continuación puede descargar la guía completa y la guía para los participantes:",
                 all: {
                     text: "Guía completa",
-                    href: "#"
+                    href: "#",
                 },
                 participants: {
                     text: "Guía para los participantes",
-                    href: "#"
-                }
-            }
-
+                    href: "#",
+                },
+            },
         ],
         pt: [
             {
                 intro: "Abaixo, você pode fazer o download do guia completo e do guia para os participantes.",
                 all: {
                     text: "Guia completo",
-                    href: "#"
+                    href: "#",
                 },
                 participants: {
                     text: "Guia para os participantes",
-                    href: "#"
-                }
-            }
-
+                    href: "#",
+                },
+            },
         ],
         fr: [
             {
                 intro: "Vous pouvez télécharger le guide complet et le guide pour les participants ci-dessous.",
                 all: {
                     text: "Guide complet",
-                    href: "#"
+                    href: "#",
                 },
                 participants: {
                     text: "Guide pour les participants",
-                    href: "#"
-                }
-            }
-
-        ]
+                    href: "#",
+                },
+            },
+        ],
     },
     images: {
         one: {
@@ -70,15 +67,11 @@ module.exports = {
             alt: {
                 en: "Bust of Socrates, Greek philosopher, in a thoughtful pose.",
                 es: "Busto de Sócrates, filósofo griego, en una pose pensativa.",
-                pt: "Busto de Sócrates, filósofo grego, em uma pose pensativa." ,
-                fr: "Buste de Socrate, philosophe grec, dans une pose pensive."
+                pt: "Busto de Sócrates, filósofo grego, em uma pose pensativa.",
+                fr: "Buste de Socrate, philosophe grec, dans une pose pensive.",
             },
-            classes: [
-                "guide-image", 
-                "f-right", 
-                "m-left"
-            ],
-            id: "cover"
+            classes: ["guide-image", "f-right", "m-left"],
+            id: "cover",
         },
         two: {
             src: "/img/recursos-didacticos/guia-socratica/la-escuela-de-atenas.png",
@@ -86,26 +79,20 @@ module.exports = {
                 en: "Fresco 'The School of Athens' showing Plato and Aristotle at the center, surrounded by other philosophers, including Socrates.",
                 es: "Fresco 'La escuela de Atenas' que muestra a Platón y Aristóteles en el centro, rodeados de otros filósofos, incluyendo a Sócrates.",
                 pt: "Fresco 'A Escola de Atenas' mostrando Platão e Aristóteles no centro, cercados por outros filósofos, incluindo Sócrates.",
-                fr: "Fresque 'L'École d'Athènes' montrant Platon et Aristote au centre, entourés d'autres philosophes, y compris Socrate."
+                fr: "Fresque 'L'École d'Athènes' montrant Platon et Aristote au centre, entourés d'autres philosophes, y compris Socrate.",
             },
-            classes: [
-                "guide-image"
-            ],
+            classes: ["guide-image"],
         },
         three: {
-            src: "/img/recursos-didacticos/guia-socratica/socrates-busto-1.png",
+            src: "/img/recursos-didacticos/guia-socratica/socrates-busto-1.jpg",
             alt: {
                 en: "Bust of Socrates, the Greek philosopher, featuring a serious expression and prominent beard.",
                 es: "Busto de Sócrates, el filósofo griego, con una expresión seria y barba prominente.",
                 pt: "Busto de Sócrates, o filósofo grego, com uma expressão séria e uma barba proeminente.",
-                fr: "Buste de Socrate, le philosophe grec, avec une expression sérieuse et une barbe proéminente."
+                fr: "Buste de Socrate, le philosophe grec, avec une expression sérieuse et une barbe proéminente.",
             },
-            classes: [
-                "guide-image",
-                "f-right",
-                "m-left"
-            ],
-            id: "bust1"
+            classes: ["guide-image", "f-right", "m-left"],
+            id: "bust1",
         },
         four: {
             src: "/img/recursos-didacticos/guia-socratica/ilustracion-1.png",
@@ -113,27 +100,20 @@ module.exports = {
                 en: "Illustration of a group of people sitting, displaying various expressions and emotions, with question and answer symbols in the background.",
                 es: "Ilustración de un grupo de personas sentadas, mostrando diferentes expresiones y emociones, con símbolos de preguntas y respuestas en el fondo.",
                 pt: "Ilustração de um grupo de pessoas sentadas, mostrando diferentes expressões e emoções, com símbolos de perguntas e respostas ao fundo.",
-                fr: "Illustration d'un groupe de personnes assises, affichant différentes expressions et émotions, avec des symboles de questions et de réponses en arrière-plan."
+                fr: "Illustration d'un groupe de personnes assises, affichant différentes expressions et émotions, avec des symboles de questions et de réponses en arrière-plan.",
             },
-            classes: [
-                "guide-image"
-            ]
+            classes: ["guide-image"],
         },
         five: {
             src: "/img/recursos-didacticos/guia-socratica/chica-pensando.png",
             alt: {
-                en:"A thoughtful woman in a yellow shirt, making a gesture of uncertainty while considering options.",
+                en: "A thoughtful woman in a yellow shirt, making a gesture of uncertainty while considering options.",
                 es: "Una mujer pensativa con una camiseta amarilla, mostrando un gesto de duda mientras considera opciones.",
                 pt: "Uma mulher pensativa com uma camiseta amarela, fazendo um gesto de dúvida enquanto considera opções.",
-                fr: "Une femme pensive en chemise jaune, faisant un geste d'incertitude tout en considérant des options."
+                fr: "Une femme pensive en chemise jaune, faisant un geste d'incertitude tout en considérant des options.",
             },
-            classes: [
-                "guide-image",
-                "w-40",
-                "f-right",
-                "m-left"
-            ],
-            id: "thinking"
+            classes: ["guide-image", "w-40", "f-right", "m-left"],
+            id: "thinking",
         },
         six: {
             src: "/img/recursos-didacticos/guia-socratica/socrates-busto-2.png",
@@ -141,15 +121,10 @@ module.exports = {
                 en: "Bust of Socrates, the Greek philosopher, with a serious expression and beard, carved in marble.",
                 es: "Busto de Sócrates, el filósofo griego, con una expresión seria y barba, tallado en mármol.",
                 pt: "Busto de Sócrates, o filósofo grego, com uma expressão séria e barba, esculpido em mármore.",
-                fr: "Buste de Socrate, le philosophe grec, avec une expression sérieuse et une barbe, sculpté dans le marbre."
+                fr: "Buste de Socrate, le philosophe grec, avec une expression sérieuse et une barbe, sculpté dans le marbre.",
             },
-            classes: [
-                "guide-image",
-                "w-40",
-                "f-right",
-                "m-left"
-            ],
-            id: "bust2"
+            classes: ["guide-image", "w-40", "f-right", "m-left"],
+            id: "bust2",
         },
         seven: {
             src: "/img/recursos-didacticos/guia-socratica/socrates-escultura.png",
@@ -157,15 +132,10 @@ module.exports = {
                 en: "Sculpture of Socrates, the Greek philosopher, sitting and holding a book in his hand, with a focused expression.",
                 es: "Escultura de Sócrates, el filósofo griego, sentado y sosteniendo un libro en la mano, con una expresión concentrada",
                 pt: "Escultura de Sócrates, o filósofo grego, sentado e segurando um livro na mão, com uma expressão concentrada.",
-                fr: "Sculpture de Socrate, le philosophe grec, assis et tenant un livre à la main, avec une expression concentrée."
+                fr: "Sculpture de Socrate, le philosophe grec, assis et tenant un livre à la main, avec une expression concentrée.",
             },
-            classes: [
-                "guide-image",
-                "w-60",
-                "f-right",
-                "m-left"
-            ],
-            id: "sculpture1"
+            classes: ["guide-image", "w-60", "f-right", "m-left"],
+            id: "sculpture1",
         },
         eight: {
             src: "/img/recursos-didacticos/guia-socratica/ilustracion-2.png",
@@ -173,14 +143,9 @@ module.exports = {
                 en: "Illustration of three people collaborating: a woman with glasses using a laptop, a man holding a tablet, and another woman pointing at a light bulb, symbolizing ideas.",
                 es: "Ilustración de tres personas colaborando: una mujer con gafas usando una laptop, un hombre sosteniendo una tablet y otra mujer señalando una bombilla, simbolizando ideas.",
                 pt: "Ilustração de três pessoas colaborando: uma mulher com óculos usando um laptop, um homem segurando um tablet e outra mulher apontando para uma lâmpada, simbolizando ideias.",
-                fr: "Illustration de trois personnes collaborant : une femme avec des lunettes utilisant un ordinateur portable, un homme tenant une tablette et une autre femme pointant une ampoule, symbolisant des idées."
+                fr: "Illustration de trois personnes collaborant : une femme avec des lunettes utilisant un ordinateur portable, un homme tenant une tablette et une autre femme pointant une ampoule, symbolisant des idées.",
             },
-            classes: [
-                "guide-image",
-                "w-50",
-                "f-right",
-                "m-left"
-            ],
+            classes: ["guide-image", "w-50", "f-right", "m-left"],
         },
         nine: {
             src: "/img/recursos-didacticos/guia-socratica/reflexion-escultura.png",
@@ -188,13 +153,10 @@ module.exports = {
                 en: "Illustration of three people collaborating: a woman with glasses using a laptop, a man holding a tablet, and another woman pointing at a light bulb, symbolizing ideas.",
                 es: "Ilustración de tres personas colaborando: una mujer con gafas usando una laptop, un hombre sosteniendo una tablet y otra mujer señalando una bombilla, simbolizando ideas.",
                 pt: "Ilustração de três pessoas colaborando: uma mulher com óculos usando um laptop, um homem segurando um tablet e outra mulher apontando para uma lâmpada, simbolizando ideias.",
-                fr: "Illustration de trois personnes collaborant : une femme avec des lunettes utilisant un ordinateur portable, un homme tenant une tablette et une autre femme pointant une ampoule, symbolisant des idées."
+                fr: "Illustration de trois personnes collaborant : une femme avec des lunettes utilisant un ordinateur portable, un homme tenant une tablette et une autre femme pointant une ampoule, symbolisant des idées.",
             },
-            classes: [
-                "guide-image",
-                "centered",
-          ],
-            id: "sculpture2"
-        }
-    }
-}
+            classes: ["guide-image", "centered"],
+            id: "sculpture2",
+        },
+    },
+};
