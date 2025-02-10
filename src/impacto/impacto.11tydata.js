e0cffbbd5f3d4ec96e81c9b7e92c313a602b5bb1
Diego <daalvarez@unbosque.edu.co>
2025-02-10 13:08:48 -0500

    Add usePlyr: true to the js dir file

--- src/impacto/impacto.11tydata.js
+++ src/impacto/impacto.11tydata.js
@@ -1,18 +1,19 @@
 module.exports = {
-  layout: "layouts/testimonial.liquid",
-  tags: ["impacto"],
-  // eleventyComputed: {
-  //   permalink: data => `/testimonios/${data.city.toLowerCase()}-${data.year}/index.html`,
-  //   videos: data => {
-  //     if (!data.videos) return null;
-  //     return data.videos.map(video => {
-  //       if (typeof video === 'string') {
-  //         return {
-  //           url: video
-  //         };
-  //       }
-  //       return video;
-  //     });
-  //   }
-  // }
+    layout: "layouts/testimonial.liquid",
+    tags: ["impacto"],
+    usePlyr: true,
+    // eleventyComputed: {
+    //   permalink: data => `/testimonios/${data.city.toLowerCase()}-${data.year}/index.html`,
+    //   videos: data => {
+    //     if (!data.videos) return null;
+    //     return data.videos.map(video => {
+    //       if (typeof video === 'string') {
+    //         return {
+    //           url: video
+    //         };
+    //       }
+    //       return video;
+    //     });
+    //   }
+    // }
 };
