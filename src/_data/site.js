0f18b15f22cce49a78b7c9e3a9892e2fe89b76dd
Diego <daalvarez@unbosque.edu.co>
2025-04-08 20:49:07 -0500

    Edit site metadata, important for seo

--- src/_data/site.js
+++ src/_data/site.js
@@ -6,10 +6,21 @@ module.exports = {
   languages: ["es"],  // En el futuro: ["es", "en", "pt"]
   
   // Información del sitio
-  name: "Irene",
-  description: "...",
+  name: "Proyecto Irene",
+  description: "Proyecto Irene: un libro de trabajo basado en la literatura clásica para la resolución de conflictos. Desarrollando habilidades de resolución de conflictos en estudiantes de secundaria a través de la literatura clásica.",
   url: "https://proyectoirene.pages.dev", // URL de producción
-  
+
+  // SEO
+  author: "Ronald Forero Álvarez, Martin Dinter, Deisy Amapola Vásquez Guerrero, Juan Gabriel Santamaría Pérez, Jeiviane Justiniano Da Silva, Anni Marcelli Santos de Jesus, Carlos Renato Rosário de Jesús, Rafael Uribe Neira, Lucio Martín Forero Álvarez, Jesús David Girado Sierra, Natalia Valentina Méndez López, Felipe Vargas Cortés",
+  keywords: "proyecto irene, educación, literatura clásica, resolución de conflictos, método socrático, paz, convivencia, diálogo",
+
+  // Social media
+  social: {
+    instagram: "irenepazcol", // Sin el @ inicial
+    // facebook: "Irene-Paz-Col",  // Nombre de usuario o ID de página
+    tiktok: "irenepazcol"     // Sin el @ inicial
+  },
+
   // Año actual para el copyright
   currentYear: new Date().getFullYear(),
 }; 
